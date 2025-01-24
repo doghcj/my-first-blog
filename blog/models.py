a73686cc76b47361e8352e73e01f067c3b40cceb
edwin <edwintatis6@gmail.com>
2025-01-24 17:57:03 -0400

    First application, primer commit

---
+++ blog/models.py
@@ -0,0 +1,23 @@
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+
+class post(models.Model):
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    create_date= models.DateTimeField(
+        default=timezone.now)
+    pusblished_date = models.DateField(
+        blank= True, null=True)
+    
+
+    def publish(self):
+        self.pusblished_date =timezone.now()
+        self.save()
+
+
+    def __str__(self):
+        return self.title   
+    
